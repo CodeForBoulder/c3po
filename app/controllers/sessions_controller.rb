6f7439184ead7396d630205dac794228606ab25c
lmnleaf <larkfleming@gmail.com>
2015-06-24 20:53:36 -0600

    adds facebook auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,9 @@ class SessionsController < ApplicationController
     redirect_to root_url
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Signed out!"
+  end
+
 end
