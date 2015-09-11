6904c1a50f6300845f93aa1bb413af9cc1348924
W Rumbley <bill.rumbley@gmail.com>
2015-09-11 15:59:23 -0600

    Added Facebook Like and Share buttons using biasport:facebook-sdk package

--- permits/permits.js
+++ permits/permits.js
@@ -2,24 +2,7 @@
 Cases = new Mongo.Collection("cases");
 var devCases = {};
 var MAP_ZOOM = 15;
-
-
-/*
-// Load the GeoJSON information
-var fs = require ('fs');
-    var cases;
-
-// Read the file and send to the callback
-fs.readFile('DevelopmentReview.GeoJSON', handleGeoJSON)
-
-// GeoJSON load callback
-function handleGeoJSON(err, data) {
-    if (err) throw err;
-    cases = JSON.parse(data);
-    
-};
-*/
-
+var curAppId ;
 
 if (Meteor.isClient) {
     // counter starts at 0
@@ -30,8 +13,17 @@ if (Meteor.isClient) {
     Meteor.startup(function() {
         GoogleMaps.load();
         $("#nav").slideUp("fast");
+
+        /* set appId for Facebook integration */
+        /* First works with ManUnderhill's localhost, second with cfbc3po.meteor.com */
+        /* as registered Facebook Apps */
+        
+        var curHref = location.href;
+        var patt = new RegExp("localhost");
+        curAppId = (patt.test(location.href)) ? '109055546115993':'1459289887712466';
+        $(".fb-like").attr("data-href",curHref);
     });
-    
+
     function showDetails(e) {
         // redo to allow for multiple cases
         // find all the cases matching the address of the selected case
@@ -100,14 +92,12 @@ HTTP.get(Meteor.absoluteUrl("/DevelopmentReview.GeoJSON"), function(err,result) 
                 position: new google.maps.LatLng(latLng.lat, latLng.lng),
                 map: map.instance
             });
-            console.log("About to load the GeoJson file");
-            console.log("map object contains:\n" + Object.getOwnPropertyNames(map.instance));
             map.instance.data.loadGeoJson('/DevelopmentReview.GeoJSON'); // place json file in /public folder
-            console.log("Loaded the GeoJson file");
             map.instance.data.addListener('click', function(event) {
                 showDetails(event);
             });
-/*
+
+            /*
             // Load the GeoJSON information
             var fs = require ('fs');
                 var cases;
@@ -125,7 +115,32 @@ HTTP.get(Meteor.absoluteUrl("/DevelopmentReview.GeoJSON"), function(err,result) 
         });
     });
     
-}
+// c3poDev for localhost:3000 operation
+
+        // set up Facebook sdk
+
+    window.fbAsyncInit = function() {
+        console.log("About to call FB.init");
+        FB.init({
+            appId      : curAppId, // set appId based on localhost vs meteor.com host
+            status     : true,
+            version    : 'v2.4',
+            xfbml      : true
+        });
+        console.log("Finished call to FB.init");
+    };
+
+//            appId      : '109055546115993', // localhost
+//            appId      : '1459289887712466', // c3poTest
+    
+
+    Template.fbbtn.onRendered(function() {
+        try {
+            FB.XFBML.parse();
+        }catch(e) {}   
+    });
+   
+};
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
